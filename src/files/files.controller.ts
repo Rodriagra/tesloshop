24681fe26d458adef2e86c0cb00c7f47686f4ecd
Rodriagra <81830930+Rodriagra@users.noreply.github.com>
2023-02-02 12:22:19 +0100

    Seccion 12

---
+++ src/files/files.controller.ts
@@ -0,0 +1,51 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  BadRequestException,
+} from '@nestjs/common';
+import { Res, UploadedFile, UseInterceptors } from '@nestjs/common/decorators';
+import { ConfigService } from '@nestjs/config';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { Response } from 'express';
+import { diskStorage } from 'multer';
+import { FilesService } from './files.service';
+import { fileFilter, fileNamer } from './helpers';
+
+@Controller('files')
+export class FilesController {
+  constructor(
+    private readonly filesService: FilesService,
+    private readonly configService: ConfigService
+    ) {}
+
+  @Get('product/:imageName')
+  findProductImage(
+    @Res() res: Response,
+    @Param('imageName') imageName: string,
+  ) {
+    const path = this.filesService.getStaticProductImage(imageName);
+    res.sendFile(path)
+  }
+
+  @Post('product')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      fileFilter: fileFilter,
+      storage: diskStorage({
+        destination: './static/products',
+        filename: fileNamer,
+      }),
+    }),
+  )
+  uploadProductImage(@UploadedFile() file: Express.Multer.File) {
+    if (!file) throw new BadRequestException('Make sure that file is a image');
+    const secureUrl = `${this.configService.get('HOST_API')}/files/product/${file.filename}`;
+
+    return {secureUrl};
+  }
+}
---
+++ src/files/files.service.ts
@@ -0,0 +1,12 @@
+import { join } from 'path';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { existsSync } from 'fs';
+
+@Injectable()
+export class FilesService {
+  getStaticProductImage(imageName: string) {
+    const path = join(__dirname, '../../static/products', imageName);
+    if (!existsSync(path)) throw new BadRequestException('Not product found');
+    return path;
+  }
+}
